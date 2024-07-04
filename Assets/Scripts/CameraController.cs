25f24f1599c0637dedc7c5fa0f53166580b91afc
zerabruck <zerabruckm@gmail.com>
2024-07-04 17:48:41 +0300

    add levitaion ability

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField]private float speed;
-    [SerializeField]private Transform playerPosition;
-    [SerializeField]private float distanceLookAhead;
-    [SerializeField]private float cameraSpeed;
-    [SerializeField]private float lookDown;
+    [SerializeField] private float speed;
+    [SerializeField] private Transform playerPosition;
+    [SerializeField] private float distanceLookAhead;
+    [SerializeField] private float cameraSpeed;
+    [SerializeField] private float lookDown;
 
     private float lookAhead;
 
@@ -14,16 +14,18 @@ public class CameraController : MonoBehaviour
     private float currentPosX;
     private Vector3 velocity = Vector3.zero;
 
-    private void Update() {
+    private void Update()
+    {
         // transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
-        transform.position = new Vector3(playerPosition.position.x + lookAhead, playerPosition.position.y + lookDown, transform.position.z);
+        transform.position = new Vector3(playerPosition.position.x + lookAhead,  transform.position.y, transform.position.z);
         lookAhead = Mathf.Lerp(lookAhead, distanceLookAhead * playerPosition.localScale.x, Time.deltaTime * cameraSpeed);
 
 
     }
-    public void MoveToNewRoom(Transform _postion){
+    public void MoveToNewRoom(Transform _postion)
+    {
         currentPosX = _postion.position.x;
     }
 
-   
+
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
     [SerializeField] private bool isReversed;
+    [SerializeField] private BoxCollider2D attackCollider;
+    [SerializeField] protected float colliderDistance;
+    [SerializeField] protected float range;
+
+
+
 
 
     private Rigidbody2D rigidbody;
@@ -16,98 +22,129 @@ public class PlayerMovement : MonoBehaviour
     private float verticalPress;
     private float ropeCollDown = 0.5f;
     private float currentCollDown;
-
-    [SerializeField]private LayerMask groundLayer;
-    [SerializeField]private LayerMask ropeLayer;
-  public float gravity = 9.8f; // Standard gravity
+    private GameObject selectedObject = null;
 
 
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask ropeLayer;
+    [SerializeField] private LayerMask rockLayer;
 
-    void FixedUpdate()
+
+
+    private void Awake()
     {
-        // Example condition to check if the object is upside down
-        if (transform.up.y < 0)
-        {
-            // Apply custom gravity upwards
-            rigidbody.AddForce(Vector2.up * gravity);
-        }
-        else
-        {
-            // Apply custom gravity downwards
-            rigidbody.AddForce(Vector2.down * gravity);
-        }
-    }
-
-    
-    private void Awake() {
         rigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         boxCollider = GetComponent<BoxCollider2D>();
         currentDoubleJumpCollDown = 0;
     }
-    private void Start() {
-        rigidbody.gravityScale = -1;
-        
+    private void MoveRock(){
+        if (selectedObject != null)
+        {
+                SpriteRenderer spriteRenderer = selectedObject.GetComponent<SpriteRenderer>();
+                if (spriteRenderer != null) {
+        spriteRenderer.color = Color.green;
     }
-    private void Update() {
-        FixedUpdate();
-        if (isGrounded()){
+
+            if (Input.GetMouseButton(0))
+            {
+                animator.SetBool("levitation", true);
+                Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                selectedObject.transform.position = mousePosition;
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                animator.SetBool("levitation", false);
+                selectedObject = null;
+            }
+        }
+
+    }
+
+    private void Update()
+    {
+        RockInsight();
+        if(selectedObject){
+            MoveRock();
+        }
+        if (isGrounded())
+        {
             currentDoubleJumpCollDown = 0;
         }
-        if(isReversed){
+        if (isReversed)
+        {
             rigidbody.gravityScale = -1;
         }
         currentCollDown += Time.deltaTime;
         horizontalPress = Input.GetAxis("Horizontal");
-        if (!ropeContact()){
-        if(horizontalPress > 0) {
-            transform.localScale = new Vector2(1.5f, transform.localScale.y);
-        } else if(horizontalPress < 0) {
-            transform.localScale = new Vector2(-1.5f, transform.localScale.y);
-        }
-        rigidbody.velocity = new Vector2(horizontalPress * speed, rigidbody.velocity.y);
-        if (isGrounded()){
-        animator.SetBool("runing", horizontalPress != 0);
-        } else {
-            animator.SetBool("runing", false);
-        }
-        }
-       
-        if (Input.GetKey(KeyCode.Space) && ropeContact()){
-                jump();
-        }
-        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded() || currentDoubleJumpCollDown <= 2)){
-                jump();
+        if (!ropeContact())
+        {
+            if (horizontalPress > 0)
+            {
+                transform.localScale = new Vector2(1.5f, transform.localScale.y);
+            }
+            else if (horizontalPress < 0)
+            {
+                transform.localScale = new Vector2(-1.5f, transform.localScale.y);
+            }
+            rigidbody.velocity = new Vector2(horizontalPress * speed, rigidbody.velocity.y);
+            if (isGrounded())
+            {
+                animator.SetBool("runing", horizontalPress != 0);
+            }
+            else
+            {
+                animator.SetBool("runing", false);
+            }
         }
 
-        if(ropeContact()){
+        if (Input.GetKey(KeyCode.Space) && ropeContact())
+        {
+            jump();
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded() || currentDoubleJumpCollDown <= 2))
+        {
+            jump();
+        }
+
+        if (ropeContact())
+        {
             currentCollDown += Time.deltaTime;
             Debug.Log("Rope contact");
             rigidbody.gravityScale = 0;
             animator.SetBool("runing", false);
             animator.SetBool("climbIdle", true);
             verticalPress = Input.GetAxis("Vertical");
-            if (verticalPress > 0){
-            animator.SetBool("climbIdle", false);
-            animator.SetBool("climb", true);
-                rigidbody.velocity = new Vector2( rigidbody.velocity.x, verticalPress * speed);
-            } else if (verticalPress < 0){
+            if (verticalPress > 0)
+            {
                 animator.SetBool("climbIdle", false);
                 animator.SetBool("climb", true);
                 rigidbody.velocity = new Vector2(rigidbody.velocity.x, verticalPress * speed);
-            } else {
+            }
+            else if (verticalPress < 0)
+            {
+                animator.SetBool("climbIdle", false);
+                animator.SetBool("climb", true);
+                rigidbody.velocity = new Vector2(rigidbody.velocity.x, verticalPress * speed);
+            }
+            else
+            {
                 rigidbody.velocity = new Vector2(rigidbody.velocity.x, 0);
                 animator.SetBool("climb", false);
-                
-            } 
-            if(horizontalPress > 0 && currentCollDown > ropeCollDown && !Input.GetKey(KeyCode.Space)) {
-            transform.localScale = new Vector2(-1.5f,transform.localScale.y);
-        } else if(horizontalPress < 0 && currentCollDown > ropeCollDown && !Input.GetKey(KeyCode.Space))  {
-            transform.localScale = new Vector2(1.5f, transform.localScale.y);
+
+            }
+            if (horizontalPress > 0 && currentCollDown > ropeCollDown && !Input.GetKey(KeyCode.Space))
+            {
+                transform.localScale = new Vector2(-1.5f, transform.localScale.y);
+            }
+            else if (horizontalPress < 0 && currentCollDown > ropeCollDown && !Input.GetKey(KeyCode.Space))
+            {
+                transform.localScale = new Vector2(1.5f, transform.localScale.y);
+            }
+
         }
-            
-        } 
-        else {
+        else
+        {
             currentCollDown = 0;
             rigidbody.gravityScale = 1;
             animator.SetBool("climb", false);
@@ -115,46 +152,74 @@ public class PlayerMovement : MonoBehaviour
 
         }
         animator.SetBool("grounded", isGrounded());
-        
+
 
     }
-    private void jump(){
-        currentDoubleJumpCollDown += 1; 
+    private void jump()
+    {
+        currentDoubleJumpCollDown += 1;
         Debug.Log(" current double jump" + currentDoubleJumpCollDown + isGrounded());
-        if(ropeContact()){
-            if (horizontalPress != 0){
-        animator.SetTrigger("jump");
-            // transform.localScale = new Vector2(-1 * transform.localScale.x, transform.localScale.y);
-            rigidbody.velocity = new Vector2(-1 * transform.localScale.x * 3, rigidbody.velocity.y + 5);}
+        if (ropeContact())
+        {
+            if (horizontalPress != 0)
+            {
+                animator.SetTrigger("jump");
+                // transform.localScale = new Vector2(-1 * transform.localScale.x, transform.localScale.y);
+                rigidbody.velocity = new Vector2(-1 * transform.localScale.x * 3, rigidbody.velocity.y + 5);
+            }
 
-            } 
-            if(Input.GetKeyDown(KeyCode.Space)){
-            if (currentDoubleJumpCollDown == 2){
-            Debug.Log("Double Jump");
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (currentDoubleJumpCollDown == 2)
+            {
+                Debug.Log("Double Jump");
                 animator.SetTrigger("doubleJump");
             }
-            if(currentDoubleJumpCollDown == 1){
+            if (currentDoubleJumpCollDown == 1)
+            {
                 animator.SetTrigger("jump");
-                }
-        rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpForce);
-
             }
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpForce);
 
-}
-        
-        
+        }
 
-        private bool isGrounded(){
-            RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size,0, Vector2.down, 0.1f, groundLayer);
-            return raycastHit.collider != null;
     }
-       private bool ropeContact(){
-            RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size,0, new Vector2(transform.localScale.x, 0), 0.1f, ropeLayer);
 
-            return raycastHit.collider != null;
+
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
     }
-    public bool canAttack(){
+    private bool ropeContact()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, ropeLayer);
+
+        return raycastHit.collider != null;
+    }
+    public bool canAttack()
+    {
         return isGrounded() && !ropeContact() && horizontalPress == 0;
+    }
+
+    public bool RockInsight()
+    {
+        // Check if player is insight
+        RaycastHit2D raycastHit = Physics2D.BoxCast(attackCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, new Vector2(attackCollider.bounds.size.x * range, attackCollider.bounds.size.y), 0, Vector2.left, 0, rockLayer);
+        if (raycastHit.collider != null)
+        {
+            // Object detected, prepare to drag
+            selectedObject = raycastHit.collider.gameObject;
+        }
+        return raycastHit.collider != null;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(attackCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, new Vector2(attackCollider.bounds.size.x * range, attackCollider.bounds.size.y));
+
     }
 }
 
